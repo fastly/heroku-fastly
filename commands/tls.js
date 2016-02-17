d2bd71832261dea2702df828942d41662e3db6f2
Michael May <michael@fastly.com>
2016-02-17 23:57:53 +0000

    add support for domain removal

--- commands/tls.js
+++ commands/tls.js
@@ -12,21 +12,37 @@ var create_domain = function(data, api_key) {
   }, function(err, response, body) {
     if (response) {
       console.log(response);
-
     }
     if (err) {
-      console.log('Error: ' + err);
+      throw new Error("Request Error: " + err);
     }
   });
 };
+
+var remove_domain = function(data, api_key) {
+  request({
+    url: base_uri + '/plugin/heroku/tls',
+    method: 'DELETE',
+    headers: { 'Fastly-Key': api_key },
+    form: data
+  }, function(err, response, body) {
+    if (response) {
+      console.log(response);
+    }
+    if (err) {
+      throw new Error("Request Error: " + err);
+    }
+  });
+};
+}
 
 module.exports = {
   topic: 'fastly',
   command: 'tls',
   description: 'Heroku CLI plugin for Fastly TLS configuration',
   help: 'Add or remove a domain for use with TLS. \n\
-   Domains will be added to a SAN certificate. For details see: https://docs.fastly.com/guides/securing-communications/ordering-a-paid-tls-option#shared-certificate \n\
-   Usage: \n\n\
+   Domains will be added to a SAN certificate. For details see: https://docs.fastly.com/guides/securing-communications/ordering-a-paid-tls-option#shared-certificate \n\n\
+   Usage: \n\
    heroku fastly:tls -c -d www.example.org -v email -a my-example-app',
   needsApp: true,
   needsAuth: true,
@@ -44,11 +60,21 @@ module.exports = {
       throw new Error("FASTLY_API_KEY not found! The Fastly add-on is required to configure TLS. Install Fastly at https://elements.heroku.com/addons/fastly");
     }
 
-    create_domain(
-      { domain: context.flags.domain,
+    if (context.flags.create) {
+      req_data = {
+        domain: context.flags.domain,
         verification_type: context.flags.verification_type
-      },
-      config.FASTLY_API_KEY);
+      }
+      create_domain(req_data, config.FASTLY_API_KEY);
+    } else if (context.flags.remove) {
+      req_data = {
+        domain: context.flags.domain,
+      }
+      remove_domain(req_data, config.FASTLY_API_KEY);
+
+    } else {
+      throw new Error("You must specify an action! Use create (-c) or remove (-r)");
+    }
   })
 
 }
