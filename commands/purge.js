e21ffdfeba601e43d2cc7250998bb7edefc3e0f9
Michael May <michael@fastly.com>
2016-03-02 01:20:04 +0000

    clean up purge command wording

--- commands/purge.js
+++ commands/purge.js
@@ -2,20 +2,15 @@
 module.exports = {
   topic: 'fastly',
   command: 'purge',
-  description: 'Purge objects from fastly edge caches',
-  help: 'Purge an object or the entire cache',
+  description: 'purge object from the cache with provided surrogate key or url',
+  help: 'purge object from the cache with given surrogate key or url',
+  args: [ { name: 'key'} ],
   flags: [
       { name: 'all', description: 'Issues a Fastly PurgeAll' }
   ],
 
   run: function (context) {
-    if (context.flags.all) {
-      // Fastly.purge_all();
-
-      console.log('PurgeAll request issued');
-    } else {
       // Fastly.purge_url();
       console.log('Purge request issued for url: ');
-    }
   }
 };
