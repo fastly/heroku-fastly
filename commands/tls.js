c029a82bad33f21a15cc18384806a067d49c7cda
Michael May <michael@fastly.com>
2016-03-11 21:02:20 +0000

    fix up tls command

--- commands/tls.js
+++ commands/tls.js
@@ -16,60 +16,64 @@ For detail on verification see (some link). \n\
  - Wildcard domains are not allowed \n\
 For details: https://docs.fastly.com/guides/securing-communications/ordering-a-paid-tls-option#shared-certificate \n\n\
 Usage: \n\
-   heroku fastly:tls www.example.org --add -v email --app my-tls-example \n\
+   heroku fastly:tls www.example.org -v email --app my-tls-example \n\
    heroku fastly:tls www.example.org --remove --app my-tls-example',
   needsApp: true,
   needsAuth: true,
   args: [ {name: 'domain'} ],
   flags: [
-        {name: 'remove', char: 'r', description: 'make a cert removal', hasValue: false},
-        {name: 'add', char: 'd', description: 'make a cert addition', hasValue: false},
-        {name: 'verification', char: 'v', description: 'The verification method to use - email, dns, or url', hasValue: true}
-    ],
-
+    {name: 'remove', char: 'r', description: 'make a cert removal', hasValue: false},
+    {name: 'verification', char: 'v', description: 'The verification method to use - email, dns, or url', hasValue: true},
+    {name: 'api_key', char: 'k', description: 'Fastly API Key (if different from the one set in your config vars)', hasValue: true}
+  ],
   run: hk.command(function* (context, heroku) {
-    let config = yield heroku.apps(context.app).configVars().info();
 
-    if (!config.FASTLY_API_KEY) {
-      throw new Error("FASTLY_API_KEY not found! The Fastly add-on is required to configure TLS. Install Fastly at https://elements.heroku.com/addons/fastly");
+    let config = yield heroku.apps(context.app).configVars().info();
+    let api_key = config.FASTLY_API_KEY
+
+    if (api_key == 'undefined' && context.flags.api_key == 'undefined') {
+      hk.error("config var FASTLY_API_KEY not found! The Fastly add-on is required to configure TLS. Install Fastly at https://elements.heroku.com/addons/fastly");
     }
 
-    if (context.flags.add) {
-      request({
-        method: 'POST',
-        url: base_uri + '/plugin/heroku/tls',
-        headers: { 'Fastly-Key': config.FASTLY_API_KEY },
-        form: {
-          domain: context.args.domain,
-          verification_type: context.flags.verification
-        }
-      }, function(err, response, body) {
-        if (response.statusCode != 200) {
-          hk.error("Fastly API request Error! code: " + response.statusCode + " msg: " + response.statusMessage);
-        } else {
-          hk.styledHeader(context.args.domain + " queued for certificate addition. The domain will not be added until you verify ownership using the provided verification type!");
-          hk.styledHeader("Please follow the instructions for " + context.flags.verification + " verification");
-        }
-      });
-
-    } else if (context.flags.remove) {
-
+    if (context.flags.remove) {
       request({
         method: 'DELETE',
         url: base_uri + '/plugin/heroku/tls',
         headers: { 'Fastly-Key': api_key },
-        form: { domain: context.args.domain }
+        form: { domain: context.args.domain,
+          service_id: config.FASTLY_SERVICE_ID
+        }
       }, function(err, response, body) {
         if (response.statusCode != 200) {
-          hk.error("Fastly API request Error! code: " + response.statusCode + " msg: " + response.statusMessage);
+          hk.error("Fastly API request Error! code: " + response.statusCode + " " + response.statusMessage + " " + body);
         } else {
-          hk.styledHeader(context.args.domain + " queued for certificate removal. This domain will no longer support Fastoku TLS");
+          hk.styledHeader("Domain " + context.args.domain + " queued for TLS removal. This domain will no longer support TLS");
         }
       });
 
-    } else {
-      throw new Error("You must specify an action with --add or --remove!");
+    } else { // domain addition is default operation
+
+      if (context.flags.verificiation == 'undefined') {
+        hk.error('You must provide a Verification Type: email, dns, url');
+      }
+
+      request({
+        method: 'POST',
+        url: base_uri + '/plugin/heroku/tls',
+        headers: { 'Fastly-Key': api_key },
+        form: {
+          domain: context.args.domain,
+          verification_type: context.flags.verification,
+          service_id: config.FASTLY_SERVICE_ID
+        }
+      }, function(err, response, body) {
+        if (response.statusCode != 200) {
+          hk.error("Fastly API request Error! code: " + response.statusCode + " " + response.statusMessage + " " + body);
+        } else {
+          hk.styledHeader("Domain " + context.args.domain + " queued for TLS addition.");
+          hk.warn("Follow the instructions for " + context.flags.verification + " domain verification to complete TLS configuration.");
+        }
+      });
     }
   })
-
-}
+};
