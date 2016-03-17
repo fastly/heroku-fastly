eea7e914f047f8117177b51a1cd74847fb931f42
Michael May <michael@fastly.com>
2016-03-17 19:09:03 +0000

    get a couple silly tests working

--- test/index.js
+++ test/index.js
@@ -1,9 +1,14 @@
 'use strict';
-let expect = require('chai').expect;
+require('chai').should();
+var index = require('../index');
 
 describe('fastly', function () {
-  it('has a clear command', function () {
+  it('has a purge command', function () {
     index.commands[0].command.should.equal('purge');
+  });
+
+  it('has a tls command', function () {
+    index.commands[1].command.should.equal('tls');
   });
 
 });
