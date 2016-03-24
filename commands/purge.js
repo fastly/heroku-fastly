0295beb9966102646fd165e7b7f6fdc79301d891
Michael May <michael@fastly.com>
2016-03-24 19:07:59 +0000

    implement basic purge command

--- commands/purge.js
+++ commands/purge.js
@@ -4,15 +4,40 @@ var hk = require('heroku-cli-util');
 module.exports = {
   topic: 'fastly',
   command: 'purge',
-  description: 'purge object from the cache with provided surrogate key or url',
-  help: 'purge object from the cache with given surrogate key or url',
-  args: [ { name: 'key'} ],
+  description: 'Purge the entire Fastly cache or any object(s) with the provided surrogate key',
+  help: 'Purge object(s) from the Fastly cache with the provided surrogate key\n\
+    More details on purge at docs.fastly.com/api/purge',
+  needsApp: true,
+  needsAuth: true,
+  args: [ { name: 'key', optional: true } ],
   flags: [
-      { name: 'all', description: 'Issues a Fastly PurgeAll' }
+      { name: 'all', description: 'Issues a Fastly PurgeAll', hasValue: false }
   ],
 
-  run: function (context) {
-      // Fastly.purge_url();
-      hk.styledHeader('Not yet implemented');
-  }
+  run: hk.command(function* (context, heroku) {
+    let config = yield heroku.apps(context.app).configVars().info();
+    var fastly = require('fastly')(config.FASTLY_API_KEY);
+
+    if (context.flags.all) {
+      fastly.purgeAll(config.FASTLY_SERVICE_ID, function(err, obj) {
+        if (err) {
+          hk.error(err);
+        } else {
+          hk.log(obj);
+
+        }
+      });
+    }
+
+    if (context.args.key) {
+      fastly.purgeKey(config.FASTLY_SERVICE_ID, context.args.key, function(err, obj) {
+        if (err) {
+          hk.error(err);
+        } else {
+          hk.log(obj);
+        }
+      });
+    }
+
+  })
 };
