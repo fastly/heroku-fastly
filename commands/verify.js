2699eb55a4d8279ff90d487de584164166fd9b96
Michael May <michael@fastly.com>
2016-04-19 19:08:40 +0000

    working verify command

--- commands/verify.js
+++ commands/verify.js
@@ -6,7 +6,7 @@ let co  = require('co');
 function* app (context, heroku) {
   let base_uri = context.flags.api_uri || 'https://app.fastly.com';
   let config = yield heroku.apps(context.app).configVars().info();
-  let api_key = context.flags.api_key || config.FASTLY_API_KEY
+  let api_key = context.flags.api_key || config.FASTLY_API_KEY;
 
   if (!api_key) {
     cli.error("config var FASTLY_API_KEY not found! The Fastly add-on is required to configure TLS. Install Fastly at https://elements.heroku.com/addons/fastly");
@@ -14,47 +14,44 @@ function* app (context, heroku) {
   }
 
   var url_params = "?domain=" + context.args.domain + "&service_id=" + config.FASTLY_SERVICE_ID;
+  var url = base_uri + '/plugin/heroku/tls/status' + url_params;
+  let approval = null;
 
-  let resp = ''
-  request({
-    method: 'GET',
-    url: base_uri + '/plugin/heroku/tls/status' + url_params,
-    headers: { 'Fastly-Key': api_key, 'Content-Type': 'application/json' }
-  },
-  function(err, response, body) {
-    if (response.statusCode != 200) {
-      cli.error(body);
-      process.exit(1);
-    }
-    else {
-      resp = body;
-      cli.log(body);
-    }
-  });
+  request.get({url: url, headers: {'Fastly-Key': api_key}}, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let json = JSON.parse(body);
+      cli.warn("Valid approval domains: " + json.approvals.toString());
 
-  cli.debug(resp);
+      cli.prompt('Type the approval domain to use (or ENTER if only 1): ').then(function (approval) {
+        let valid = json.approvals.indexOf(approval)
 
-  //let approval = yield cli.prompt('which domain?');
-  //cli.debug(approval);
-
-  request({
-    method: 'POST',
-    url: base_uri + '/plugin/heroku/tls/verify',
-    headers: { 'Fastly-Key': api_key, 'Content-Type': 'application/json' },
-    form: {
-      approval: approval,
-      domain: context.args.domain,
-      service_id: config.FASTLY_SERVICE_ID
-    }
-    }, function(err, response, body) {
-        console.log(body);
-
-        if (response.statusCode != 200) {
-          cli.error(body);
+        if (valid == -1) {
+          cli.error("Entered domain does not match a valid approval. Try again");
+          process.exit(1);
         }
 
-      }
-  )
+        request({
+          method: 'POST',
+          url: base_uri + '/plugin/heroku/tls/verify',
+          headers: { 'Fastly-Key': api_key },
+          form: {
+            approval: approval,
+            domain: context.args.domain,
+            service_id: config.FASTLY_SERVICE_ID
+          }
+        }, function(err, response, body) {
+          if (!error && response.statusCode == 200) {
+            cli.warn("Domain queued for verification");
+          } else { //POST req error
+            cli.error(body);
+          }
+        });
+      });
+    }
+    else { //GET req error
+      cli.error(body);
+    }
+  });
 
 }
 
@@ -66,7 +63,7 @@ module.exports = {
   needsApp: true,
   needsAuth: true,
   args: [
-    {name: 'domain', description: 'the domain to add TLS', optional: false}
+    {name: 'domain', description: 'The domain to verify', optional: false}
   ],
   flags: [
     {name: 'api_key', char: 'k', description: 'Override Fastly_API_KEY config var', hasValue: true},
